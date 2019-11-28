917b2648b12bceed5f150cd59b53a850d65bbae5
Tiia Toivonen <tiiatoiv@metropolia.fi>
2019-11-28 15:56:57 +0200

    created a new repo for node2019, moving files, models

---
+++ model/animal.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const connection = require('./db');
+
+exports.getAll = async () => {
+    try {
+        const [results, fields] = await connection.query('SELECT * FROM animal');
+        console.log(results); // results contains rows r
+        console.log(fields); // fields contains extra m
+        return results;
+    } catch (e) {
+        console.log(e);
+        throw 'db error :(';
+    }
+};
+
+exports.search = async (name) => {
+    try {
+        const [results] = await connection.query(
+            'SELECT * FROM animal WHERE name LIKE ?',
+            [name]);
+        return results;
+    } catch(e) {
+        console.log(e);
+        throw `db error`;
+    }
+};
+
+exports.insert = async (name) => {
+    try {
+        const [result] = await connection.query(
+            'INSERT INTO animal  (name) VALUES (?)',
+            [name]
+        );
+        return result;
+    } catch (e) {
+        console.log(e);
+        throw('db error');
+    }
+
+};
